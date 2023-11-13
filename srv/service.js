9811e84145324e779a30b8f6e7c594d863655b92
Artem Rusetskyi <artem.rusetskyi@gmail.com>
2023-11-13 09:39:31 +0000

    Navigation + csv data

---
+++ srv/service.js
@@ -0,0 +1,8 @@
+const cds = require('@sap/cds');
+const db = cds.db;
+
+module.exports = cds.service.impl(async function () {
+
+    let { ValidationExecution, CheckSet, ScopeOfCheck, ValidationExecParamValues, ValidationExecMessages } = this.entities;
+
+})
